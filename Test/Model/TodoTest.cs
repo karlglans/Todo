d8347e21fc1f4766cd7cf927fbf03c35308081d7
Karl Glans <karlglans@users.noreply.github.com>
2021-10-16 11:06:25 +0200

    model todo (#2)

---
+++ Test/Model/TodoTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using Todo.Model;
+
+namespace Test.Model
+{
+    public class TodoTest
+    {
+        private int someSerial = 1;
+        private string someValidString = "someValidString";
+
+        [Fact]
+        public void CanMakeObject()
+        {
+            var todo = new Todo.Model.Todo(someSerial, someValidString);
+            Assert.NotNull(todo);
+            Assert.Equal(someSerial, todo.TodoId);
+        }
+    }
+}
---
+++ Todo/Model/Todo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Model
+{
+    public class Todo
+    {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public Todo(int todoId, string description)
+        {
+            this.todoId = todoId;
+            Description = description;
+        }
+
+        public int TodoId
+        {
+            get { return todoId; }
+        }
+
+        public Person Assignee { get => assignee; set => assignee = value; }
+        public bool Done { get => done; set => done = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
