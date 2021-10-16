0bc899f328a09277b023bfb4b3061f143b4fb277
Karl Glans <karlglans@users.noreply.github.com>
2021-10-16 10:27:44 +0200

    Person Model (#1)

---
+++ Test/Model/PersonTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+using Todo.Model;
+
+namespace Test.Model
+{
+    public class PersonTest
+    {
+        private int someSerial = 1;
+        private string someValidName = "someValidName";
+        private Person person;
+
+        public PersonTest()
+        {
+            person = new Person(someSerial);
+            Assert.NotNull(person);
+        }
+
+        [Fact]
+        public void CanMakeObject()
+        {
+            Person person = new Person(someSerial);
+            Assert.NotNull(person);
+        }
+
+        [Fact]
+        public void SetFirstName_canSetAValidFirstName()
+        {
+            person.FirstName = someValidName;
+            Assert.Equal(someValidName, person.FirstName);
+        }
+
+        [Fact]
+        public void SetFirstName_willNotAllowEmpyString()
+        {
+            Assert.Throws<ArgumentException>(() => person.FirstName = string.Empty);
+        }
+
+        [Fact]
+        public void SetFirstName_willNotAllowNull()
+        {
+            Assert.Throws<ArgumentException>(() => person.FirstName = null);
+        }
+
+        [Fact]
+        public void SetLastName_canSetAValidFirstName()
+        {
+            person.LastName = someValidName;
+            Assert.Equal(someValidName, person.LastName);
+        }
+
+        [Fact]
+        public void SetLastName_willNotAllowEmpyString()
+        {
+            Assert.Throws<ArgumentException>(() => person.LastName = string.Empty);
+        }
+
+        [Fact]
+        public void SetLastName_willNotAllowNull()
+        {
+            Assert.Throws<ArgumentException>(() => person.LastName = null);
+        }
+    }
+}
---
+++ Todo/Model/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Model
+{
+	public class Person
+	{
+		private readonly int personId;
+		private string firstName;
+		private string lastName;
+
+		public Person(int personId)
+		{
+			this.personId = personId;
+		}
+
+		public int PersonId
+		{
+			get { return personId; }
+		}
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set
+			{
+				if (!String.IsNullOrEmpty(value)) firstName = value;
+				else throw new ArgumentException("missing first name");
+			}
+		}
+
+		public string LastName
+		{
+			get { return lastName; }
+			set
+			{
+				if (!String.IsNullOrEmpty(value)) lastName = value;
+				else throw new ArgumentException("missing last name");
+			}
+		}
+	}
+
+}
