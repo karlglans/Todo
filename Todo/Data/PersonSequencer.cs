bce18175d2d41815a87bcfbc0a7133f4ff18887c
Karl Glans <karlglans@users.noreply.github.com>
2021-10-17 14:21:14 +0200

    Data.PersonSequencer (#3)

---
+++ Todo/Data/PersonSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Data
+{
+    public class PersonSequencer
+    {
+        static private int personId;
+
+        public static int NextPersonId()
+        {
+            return ++personId;
+        }
+
+        public static void Reset()
+        {
+            personId = 0;
+        }
+    }
+}
