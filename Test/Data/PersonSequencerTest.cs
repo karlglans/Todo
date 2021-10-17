924a63dcc6d5d9b9b00eecc2f170635bdc2df555
karlglans <karl_glans@hotmail.com>
2021-10-17 14:20:33 +0200

    Data.PersonSequencer

---
+++ Test/Data/PersonSequencerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using Todo.Data;
+
+namespace Test.Data
+{
+    public class PersonSequencerTest
+    {
+        public PersonSequencerTest()
+        {
+            PersonSequencer.Reset();
+        }
+
+        [Fact]
+        public void NextPersonId()
+        {
+            int expectedFirstValue = 1;
+            Assert.Equal(expectedFirstValue, PersonSequencer.NextPersonId());
+            Assert.Equal(expectedFirstValue + 1, PersonSequencer.NextPersonId());
+        }
+    }
+}
