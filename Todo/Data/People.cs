e520b82852dce3e5541b767a0cc594f31f6f89e7
karlglans <karl_glans@hotmail.com>
2021-10-17 18:04:08 +0200

    Data.People

---
+++ Todo/Data/People.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Model;
+
+namespace Todo.Data
+{
+    public class People
+    {
+        private static Person[] people = new Person[0];
+
+        public int Size()
+        {
+            return people.Length;
+        }
+
+        public Person[] FindAll()
+        {
+            return people;
+        }
+
+        public Person FindById(int personId)
+        {
+            // TODO: add binary search tree since personId is increasing in array 
+            foreach (Person person in people)
+            {
+                if (person.PersonId == personId) return person;
+            }
+            return null;
+        }
+
+        private void AddPersonToPeople(Person person)
+        {
+            int lastSize = people.Length;
+            Array.Resize(ref people, lastSize + 1);
+            people[lastSize] = person; // will put the person in the last position
+        }
+
+        public Person CreatePerson(string firstName, string lastName)
+        {
+            int personId = PersonSequencer.NextPersonId();
+            Person person = new Person(personId);
+            person.FirstName = firstName;
+            person.LastName = lastName;
+            AddPersonToPeople(person);
+            return person;
+        }
+
+        public void Clear()
+        {
+            Array.Resize<Person>(ref People.people, 0);
+        }
+    }
+}
